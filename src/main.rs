b3e86bb6c6c6393b7ad695cb4817758e313471ba
Andreas Öhman <andreas.ohman@>
2023-03-05 21:24:23 +0100

    Add argument parser for ROM

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,27 @@ use std::io::{stdin, Write};
 use termion::input::TermRead;
 mod i8080;
 mod disassembler;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
-    let test: u8 = 0x3e;
-    let o: u8 = 0x3e;
-    let (test, overflow) = test.overflowing_sub(o);
-    println!("{}, {}", test, overflow);
 
-    if overflow {
-        println!("{}", test as u8);
+    let args: Vec<String> = env::args().collect();
+    let mut rom = "".to_string();
+
+    if args.len() >= 2
+    {
+        rom.push_str(&env::current_dir().unwrap().to_string_lossy().to_string());  
+        rom.push_str("/");
+        rom.push_str(&args[1].to_string());
+    }
+    else
+    {
+        println!("Input path to ROM");
+        println!("./siri8080 PATH_TO_ROM");
+        return;
     }
 
-    let mut p = i8080::Processor::from(
-        "/Users/ohman/projects/siri8080/roms/cpudiag.bin".to_string(),
-    );
+    let mut p = i8080::Processor::from(rom);
 
     let mut stdin = termion::async_stdin().keys();
     let mut run = false;
