adf929ee5f9b3a3a4653866c55669b9173cdf799
Andreas Öhman <andreas.ohman@>
2023-04-30 22:40:58 +0200

    Test carry flag in ADD op

--- tests/test_arithmetic.rs
+++ tests/test_arithmetic.rs
@@ -3,12 +3,17 @@ mod tests
 {
     use siri8080::i8080::registers::*;
     use siri8080::i8080::Processor;
+    use rand::Rng;
 
     #[test]
     fn add()
     {
+        let mut rng = rand::thread_rng();
         let mem = vec![0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86]; 
-        let numbers = vec![0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07];
+        let numbers: Vec<u8> = (0..7)
+            .map(|_| rng.gen_range(1..255))
+            .collect();
+
         let mut cpu = Processor::from_bytes(mem);
         let regs = Registers {
             accumulator: 0,
@@ -25,13 +30,15 @@ mod tests
         let addr = (regs.h as u16) << 8 | (regs.l as u16);
         cpu.set_memory_at(addr, numbers[6]);
         
-        let mut sum = 0;
+        let mut sum: u8 = 0;
+        let mut carry: bool; 
+
         for i in 0..=6 {
             cpu.clock();
             let accumulator = cpu.get_registers().accumulator;
-            sum += numbers[i];
-            println!("i {}", i);
-            println!("accumulator {} sum {}", accumulator, sum);
+            (sum, carry)  = sum.overflowing_add(numbers[i]);
+            let flags = cpu.get_flags();
+            assert_eq!(flags.carry_flag, carry);
             assert_eq!(accumulator, sum);
         }
     }
